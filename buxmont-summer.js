4bc480691877676a98ef4e0a03f0b8ea6ad57ffb
Theo Fleck <emailtfleck@gmail.com>
2018-12-22 20:55:22 -0500

    change layout, improve extensibility
    Added header & footer
    Only main section replaced by templates
    Added error to put when template can't be retrieved
    Cleaned up JS flow

--- buxmont-summer.js
+++ buxmont-summer.js
@@ -1,30 +1,32 @@
-function calculateScores(event){
-  var pts = [5,3,1];
-  var rpts = [7,0,0];
+function setupRuleset() {
+  $("#distTimesBtn").html("Champs Times");
+  $("#rulesetButton").html("BuxMont"); //Short Name
+}
+
+function calculateScores(event) {
+  var pts = [5, 3, 1];
+  var rpts = [7, 0, 0];
   var num = $(event.currentTarget).children(".placed").length;
-  if($(event.target).hasClass("dq")){
+  if ($(event.target).hasClass("dq")) {
     num -= 1;
     $(event.target).removeClass("dq");
     $(event.target).addClass("not-selected");
     $(event.target).removeClass("btn-danger");
     $(event.target).addClass("btn-outline-primary");
-  }
-  else if($(event.target).hasClass("placed")){
+  } else if ($(event.target).hasClass("placed")) {
     num -= 1;
     $(event.target).removeClass("placed");
     $(event.target).addClass("dq");
     $(event.target).removeClass("btn-success");
     $(event.target).addClass("btn-danger");
-  }
-  else{
+  } else {
     num += 1;
-    if(num > 3){
+    if (num > 3) {
       $(event.target).removeClass("not-selected");
       $(event.target).addClass("dq");
       $(event.target).removeClass("btn-outline-primary");
       $(event.target).addClass("btn-danger");
-    }
-    else{
+    } else {
       $(event.target).removeClass("not-selected");
       $(event.target).addClass("placed");
       $(event.target).removeClass("btn-outline-primary");
@@ -33,75 +35,73 @@ function calculateScores(event){
   }
   var isRelay = $(event.currentTarget).parents("tr").hasClass("relay"); //check if event is a relay
   var dqd = $(event.currentTarget).children(".dq");
-  for(var i=0; i<dqd.length; i++){
-    if(isRelay){
-      rpts[parseInt($(dqd[i]).text())-1] = 0;
-    } else{
-      pts[parseInt($(dqd[i]).text())-1] = 0; 
+  for (var i = 0; i < dqd.length; i++) {
+    if (isRelay) {
+      rpts[parseInt($(dqd[i]).text()) - 1] = 0;
+    } else {
+      pts[parseInt($(dqd[i]).text()) - 1] = 0;
     }
   }
   var placed = $(event.currentTarget).children(".placed");
   var currPts = 0;
   var oppPts = 0;
   var isRelay = $(event.currentTarget).parents("tr").hasClass("relay"); //check if event is a relay
-  for(var i=0; i<placed.length; i++){
-    if($(placed[i]).text() == "1"){
-      if(isRelay){
+  for (var i = 0; i < placed.length; i++) {
+    if ($(placed[i]).text() == "1") {
+      if (isRelay) {
         currPts += rpts[0];
-      } else if(currPts < (pts[1]+pts[2])){ //Already scored 2nd and 3rd place
+      } else if (currPts < (pts[1] + pts[2])) { //Already scored 2nd and 3rd place
         currPts += pts[0];
-      } else{
-        currPts = pts[0]+pts[1];
+      } else {
+        currPts = pts[0] + pts[1];
       }
-    }
-    else if($(placed[i]).text() == "2"){
-      if(isRelay){
+    } else if ($(placed[i]).text() == "2") {
+      if (isRelay) {
         currPts += rpts[1];
-      } else if(currPts < (pts[0]+pts[2])){ //Already scored 2nd and 3rd place
+      } else if (currPts < (pts[0] + pts[2])) { //Already scored 2nd and 3rd place
         currPts += pts[1];
       } else {
-        currPts = pts[0]+pts[1];
+        currPts = pts[0] + pts[1];
       }
-    }
-    else if($(placed[i]).text() == "3"){
-      if(isRelay){
+    } else if ($(placed[i]).text() == "3") {
+      if (isRelay) {
         currPts += rpts[2];
-      } else if(currPts < (pts[0]+pts[1])){ //Already scored 2nd and 3rd place
+      } else if (currPts < (pts[0] + pts[1])) { //Already scored 2nd and 3rd place
         currPts += pts[2];
-      } else{
-        currPts = pts[0]+pts[1];
+      } else {
+        currPts = pts[0] + pts[1];
       }
     }
   }
-  if(placed.length == 0 && dqd.length == 0){
+  if (placed.length == 0 && dqd.length == 0) {
     currPts = 1;
   }
-  if(isRelay){
-    oppPts = (rpts[0]+rpts[1]+rpts[2])-currPts;
+  if (isRelay) {
+    oppPts = (rpts[0] + rpts[1] + rpts[2]) - currPts;
   } else {
-    oppPts = (pts[0]+pts[1]+pts[2])-currPts;
+    oppPts = (pts[0] + pts[1] + pts[2]) - currPts;
   }
   var yourOldPts = parseInt($("#homeScore").text());
   var oppOldPts = parseInt($("#awayScore").text());
-  var attrPts =  $(event.currentTarget).parents("tr").attr("pts");
+  var attrPts = $(event.currentTarget).parents("tr").attr("pts");
   var attrOppPts = $(event.currentTarget).parents("tr").attr("oppPts");
   var fade = false;
-  if(attrPts === undefined){
+  if (attrPts === undefined) {
     attrPts = 0;
     fade = true;
   }
-  if(attrOppPts === undefined){
+  if (attrOppPts === undefined) {
     attrOppPts = 0;
     fade = true;
   }
   yourOldPts -= attrPts;
   oppOldPts -= attrOppPts;
-  $(event.currentTarget).parents("tr").attr("pts",currPts);
-  $(event.currentTarget).parents("tr").attr("oppPts",oppPts);
-  $("#homeScore").text(yourOldPts+currPts);
-  $("#awayScore").text(oppOldPts+oppPts);
-  if(fade){
-    $(event.currentTarget).parents("tr").delay(2000).fadeOut(500,function() {
+  $(event.currentTarget).parents("tr").attr("pts", currPts);
+  $(event.currentTarget).parents("tr").attr("oppPts", oppPts);
+  $("#homeScore").text(yourOldPts + currPts);
+  $("#awayScore").text(oppOldPts + oppPts);
+  if (fade) {
+    $(event.currentTarget).parents("tr").delay(2000).fadeOut(500, function() {
       $(event.currentTarget).parents("table").append($(event.currentTarget).parents("tr"));
       $(event.currentTarget).parents("tr").fadeIn(300);
     });
--- main.js
+++ main.js
@@ -1,69 +1,83 @@
 jQuery(document).ready(function($) {
-  var cookies = (document.cookie).split(";");
-  var ruleset = "piaa-winter";
-  var times = "girls";
-  for(var i=0; i<cookies.length; i++){
-    var str = cookies[i].trim();
-    var rulesetIndex = str.indexOf("ruleset=");
-    var timesIndex = str.indexOf("times=");
-    if(rulesetIndex != -1){
-      ruleset = str.substring(rulesetIndex+8);
-    }
-    if(timesIndex != -1){
-      times = str.substring(timesIndex+6);
-    }
-  }
-  $.get(ruleset+'.html',function(data) {
-    $("#container").after(data);
+  var cookies = readCookies();
+  loadRuleset(cookies[0], cookies[1]);
+});
+
+function loadRuleset(ruleset, times) {
+  var request = $.get(ruleset + '.html');
+  request.done(function(data) {
+    $("#container").html(data);
+    setupRuleset();
     addListeners();
-    updateCookies(ruleset,times);
-    if(times == "girls"){
+    updateCookies(ruleset, times);
+    if (times == "girls") {
       $("#showGirlsTimes").trigger("click");
-    }
-    else{
+    } else {
       $("#showBoysTimes").trigger("click");
     }
   });
-});
+  request.fail(function(error) {
+    $("#container").html(`
+            <div class="alert alert-danger" role="alert">
+                There was an error trying to load the scoreboard, try reloading or selecting another scoreboard from the dropdown
+            </div>`);
+    console.log("error loading ruleset");
+    addListeners();
+    updateCookies(ruleset, times);
+  });
+}
 
-function addListeners(){
+function addListeners() {
   $(".btn-place-group").click(function(event) {
     event.preventDefault();
     calculateScores(event);
   });
   $(".ruleset-item").click(function(event) {
-    var cookie = "ruleset="+$(event.currentTarget).attr("ruleset")+";";
-    expires = new Date(new Date().getTime() + 7 * 1000 * 60 * 60 * 24);
-    cookie += "expires=" + expires.toGMTString() + ";";
-    document.cookie = cookie;
-    location.reload();
+    $("#rulesetChoices").find("i").remove();
+    $(event.currentTarget).html($(event.currentTarget).attr("long-name") + `<i class="fas fa-check"></i>`);
+    var cookies = readCookies();
+    loadRuleset($(event.currentTarget).attr("ruleset"), cookies[1]);
   });
   $("#showGirlsTimes").click(function(event) {
     $("#boysDistrictTimes").fadeOut(300, function(event) {
       $("#girlsDistrictTimes").fadeIn(300);
     });
-    var cookie = "times=girls;";
-    expires = new Date(new Date().getTime() + 7 * 1000 * 60 * 60 * 24);
-    cookie += "expires=" + expires.toGMTString() + ";";
-    document.cookie=cookie;
+    var cookies = readCookies();
+    updateCookies(cookies[0], "girls");
   });
   $("#showBoysTimes").click(function(event) {
     $("#girlsDistrictTimes").fadeOut(300, function(event) {
       $("#boysDistrictTimes").fadeIn(300);
     });
-    var cookie = "times=boys;";
-    expires = new Date(new Date().getTime() + 7 * 1000 * 60 * 60 * 24);
-    cookie += "expires=" + expires.toGMTString() + ";";
-    document.cookie=cookie;
+    var cookies = readCookies();
+    updateCookies(cookies[0], "boys");
   });
 }
 
-function updateCookies(ruleset,times){
-  var cookie = "times="+times+";";
+function readCookies() {
+  var cookies = (document.cookie).split(";");
+  var ruleset = "piaa-winter";
+  var times = "girls";
+  for (var i = 0; i < cookies.length; i++) {
+    var str = cookies[i].trim();
+    var rulesetIndex = str.indexOf("ruleset=");
+    var timesIndex = str.indexOf("times=");
+    if (rulesetIndex != -1) {
+      ruleset = str.substring(rulesetIndex + 8);
+    }
+    if (timesIndex != -1) {
+      times = str.substring(timesIndex + 6);
+    }
+  }
+  return [ruleset, times];
+}
+
+function updateCookies(ruleset, times) {
+  var cookie = "times=" + times + ";";
   expires = new Date(new Date().getTime() + 7 * 1000 * 60 * 60 * 24);
   cookie += "expires=" + expires.toGMTString() + ";";
   document.cookie = cookie;
-  cookie = "ruleset="+ruleset+";";
+  cookie = "ruleset=" + ruleset + ";";
   cookie += "expires=" + expires.toGMTString() + ";";
   document.cookie = cookie;
 }
